b084966716889678745f3c484bb380d3d9695c5e
The-JSI <shabbirsethji25@gmail.com>
2025-04-23 18:11:04 +0530

    Update compound.py

--- compound.py
+++ compound.py
@@ -8,8 +8,8 @@ def details():
     months = int(input("enter the no. of months: "))
     return true_percent, months
 
-def compound(principal, true_percent, months):
-    result = principal * (true_percent **  months)
+def compound(amount, true_percent, months):
+    result = amount * (true_percent **  months)
     return result
 
 def main():
