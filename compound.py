d8a9bd42440b2b9f9c26686bb6f0060c846c6c1d
The-JSI <shabbirsethji25@gmail.com>
2025-04-23 18:04:38 +0530

    Update compound.py

--- compound.py
+++ compound.py
@@ -5,8 +5,8 @@ def principle():
 def details():
     percent = float(input("enter the percentage: "))
     true_percent = 1 + (percent / 100)
-    period = int(input("enter the no. of months: "))
-    return true_percent, period
+    months = int(input("enter the no. of months: "))
+    return true_percent, months
 
 def compound(principal, true_percent, period):
     result = principal * (true_percent ** period)
@@ -14,8 +14,8 @@ def compound(principal, true_percent, period):
 
 def main():
     amount = principle()
-    true_percent, period = details()
-    final = compound(amount, true_percent, period)
+    true_percent, months = details()
+    final = compound(amount, true_percent, months)
     print(f"Final amount: {final}")
 
 if __name__ == '__main__':
